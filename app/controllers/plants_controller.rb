50bc8841ec47f93afada2b39d9c8f2698ef7de0e
martin mithi <gathitumithi9000@gmail.com>
2022-11-27 11:33:05 +0300

    Completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,22 @@
+class PlantsController < ApplicationController
+    wrap_parameters format: []
+    def index
+        render json: Plant.all, except:[:created_at, :updated_at], status: :ok
+    end
+
+    def show
+        plant=Plant.find_by(id: params[:id])
+        render json: plant, except: [:created_at, :updated_at]
+    end
+
+    def create
+        plant=Plant.create(plants_params)
+        render json: plant, except: [:created_at, :updated_at], status: :created
+    end
+
+    private
+
+    def plants_params
+        params.permit(:name, :image, :price)
+    end
+end
